18356664846206b211b0e60b63ba769b39abaec0
a <lucialopmig7@gmail.com>
2023-03-08 17:55:06 +0100

    Escena Exploracion Inicio

---
+++ Gastroiberia-LaRioja/Assets/Scripts/Enviroment/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+       
+        //if (DialogueManager.isActive == true)  //no se mueve el eprsonaje hasta que deje de hablar
+        //    return;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //Reset moveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        //Swap sprite direction
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Make sure we can move in this direcion by casting a box first, if the box returns null, we are free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Move
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Move
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
+
+    }
+}
