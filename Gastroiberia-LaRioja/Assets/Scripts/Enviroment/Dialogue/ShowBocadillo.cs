8c1dfad89838074bff88fc11529595e2010e5fbf
a <lucialopmig7@gmail.com>
2023-03-26 17:03:12 +0200

    cambios Dialogo y NPC

---
+++ Gastroiberia-LaRioja/Assets/Scripts/Enviroment/Dialogue/ShowBocadillo.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowBocadillo : MonoBehaviour
+{
+    public GameObject bocadillo;
+  private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") == true)
+        {
+            Debug.Log("Enter");
+            bocadillo.SetActive(true);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") == true)
+        {
+            Debug.Log("Exit");
+            bocadillo.SetActive(false);
+        }
+    }
+}
